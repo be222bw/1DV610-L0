023bbbe9d7773293fe3abb1b62b3c20cd743830d
Björn Elmqvist <be222bw@student.lnu.se>
2022-09-04 19:09:34 +0200

    Calculate the mean value of each character, and show the corresponding Unicode character.

--- src/Hello.java
+++ src/Hello.java
@@ -5,7 +5,17 @@ public class Hello {
     Scanner scanner = new Scanner(System.in);
     System.out.print("Input your name: ");
     String name = scanner.nextLine();
+
+    int sum = 0;
+    for (int i = 0; i < name.length(); i++) {
+      sum += (int) name.charAt(i);
+    }
+    int mean = sum / name.length();
+
     scanner.close();
-    System.out.println("Hello, " + name + "!");
+    System.out.println("Hello, " + name + ".");
+    System.out.println("The mean value of your name is " +
+      mean + ", whose corresponding Unicode character is '" +
+      (char) mean + "'.");
   }
 }
